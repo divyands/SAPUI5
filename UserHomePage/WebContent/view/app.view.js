017a555af2288d721be044b4dd32cc52cd85c491
divyands <divvubarbiedoll@gmail.com>
2017-09-01 11:23:58 +0530

    Update app.view.js

--- UserHomePage/WebContent/view/app.view.js
+++ UserHomePage/WebContent/view/app.view.js
@@ -1,1 +1,18 @@
+sap.ui.jsview("UserHomePage.WebContent.view.app", {
+	getControllerName : function() {
+		return "UserHomePage.WebContent.view.app";
+	},
+	createContent : function(oController) {
 
+		this.app = new sap.m.App();
+		var master = sap.ui.xmlview("master",
+				"UserHomePage.WebContent.view.Master");
+		// var Detail = sap.ui.xmlview("Detail", "Demo.WebContent.view.Detail");
+		master.getController().nav = this.getController();
+		// Detail.getController().nav = this.getController();
+		this.app.addPage(master, true);
+		// this.app.addPage(Detail, false);
+		return this.app;
+	}
+
+});
